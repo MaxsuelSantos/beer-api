514afe9bdb278daf7c5e8b52b3bb5758a5e785d1
Maxsuel Santos <santosmaxsuel17@gmail.com>
2021-04-16 15:11:26 -0300

    Initial project

---
+++ src/main/java/br/com/max/beerapi/BeerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.max.beerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/max/beerapi/entity/Beer.java
@@ -0,0 +1,30 @@
+package br.com.max.beerapi.entity;
+
+import br.com.max.beerapi.enums.BeerType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false)
+    private String brand;
+    @Column(nullable = false)
+    private int max;
+    @Column(nullable = false)
+    private int quantity;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BeerType type;
+}
